7dae3e5e5764903235a0b3bec7ad8e6f860661fd
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2021-11-13 16:16:44 +0530

    Service worker bugs fixed

--- view/public/sw.js
+++ view/public/sw.js
@@ -1,12 +1,4 @@
-const static_assets =[
-    require('./assets/fonts/ComicNeue-Regular.ttf').default,
-    require('./assets/fonts/ComicNeue-Bold.ttf').default,
-    require('./assets/icons/back_arrow.svg').default,
-    require('./assets/player-icons/o.svg').default,
-    require('./assets/player-icons/x.svg').default,
-    require('./assets/sounds/move.ogg').default,
-    require('./assets/sounds/player_join.ogg').default
-]
+
 const cache_name = 'cache-v1';
 
 const static_files = [
@@ -16,9 +8,8 @@ const static_files = [
     '/static/js/bundle.chunk.js',
     '/static/js/main.chunk.js',
     '/logo.png',
-    ...static_assets
 ];
-const self = this;
+
 
 self.addEventListener(
     'install', (event) => {
@@ -40,10 +31,9 @@ self.addEventListener(
     )
     //
 async function fetchResponse(event) {
-    console.log(event);
     const cachedResponse = await caches.match(event.request);
-    if (cachedResponse) { console.log("Cached Response");; return cachedResponse; }
-    //? Do offline and no-cache stuff
+    if (cachedResponse) {  return cachedResponse; }
+    //? Do online and no-cache stuff
     return fetch(event.request)
         .then(res => {
             let res_clone = res.clone();
--- view/src/index.tsx
+++ view/src/index.tsx
@@ -3,8 +3,6 @@ import './index.css';
 import App from './Containers/App';
 import {Provider} from 'react-redux'
 import store from './Redux/store'
-import swDev from './swDev';
-swDev();   
 ReactDOM.render(
   <Provider store={store}>
     <App />
